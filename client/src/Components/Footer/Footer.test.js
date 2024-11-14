77748c0db4aa1407ffa08a0c5836de1a926cff38
BenjaminBoisedu <benjaminboisedu354@gmail.com>
2024-11-13 19:45:36 -0500

    Login Register

--- client/src/Components/Footer/Footer.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Footer from './Footer';
-
-test('renders Footer component', () => {
-  const { getByText } = render(<Footer />);
-  const linkElement = getByText(/Footer/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- client/src/Components/Nav/Navbar.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Navbar from './Navbar';
-
-test('renders Navbar component', () => {
-  const { getByText } = render(<Navbar />);
-  const linkElement = getByText(/Navbar/i);
-  expect(linkElement).toBeInTheDocument();
-});
