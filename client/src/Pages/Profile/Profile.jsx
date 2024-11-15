de7156f670f214be7f9d11c23c59a73d34c306ea
BenjaminBoisedu <benjaminboisedu354@gmail.com>
2024-11-15 11:38:52 -0500

    Profilie

---
+++ client/src/Pages/Profile/Profile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Profile() {
+  return (
+    <div className="containerProfil">
+      <div className="profil">
+        <h1>Profil</h1>
+      </div>
+    </div>
+  );
+}
