9ab588aae0dcd353b668fa3cdb9f220594ccc2b8
ewen <66205491+ewenman5137@users.noreply.github.com>
2024-11-20 15:40:52 -0500

    fin de la V1

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../assets/styles/Home.css";
 
 export default function Home() {
   return (
@@ -7,32 +8,36 @@ export default function Home() {
         <img src="src/assets/images/Home/imageHome.png" alt="image" />
       </div>
       <div className="bloc">
-        <h1>Master Your Kitchen with SmartPantry</h1>
-        <p>Transform the way you manage your food inventory. Our app helps you track ingredients and discover delicious recipes tailored to what you have on hand.</p>
-        <div>
-          <a href="">Download</a>
-          <a href="">Learn More</a>
+        <div id="containeurText">
+          <h1>Master Your Kitchen with SmartPantry</h1>
+          <p>Transform the way you manage your food inventory. Our app helps you track ingredients and discover delicious recipes tailored to what you have on hand.</p>
+          <div id="containeurBtn">
+            <a href="">Download</a>
+            <a id="btnLearnMore" href="">Learn More</a>
+          </div>
         </div>
-        <div>
-          <img src="src/assets/images/Home/sel.png" alt="" />
-          <img src="src/assets/images/Home/carbonadeFlamande.png" alt="" />
+        <div id="containeurImageRecette">
+          <img id="imageRecette" src="src/assets/images/Home/recette.png" alt="" />
+          <img id="imagePoivre" src="src/assets/images/Home/sel.png" alt="" />
         </div>
       </div>
-      <div className="bloc">
-          <div id="text">
+      <div id="blocTextFeature">
+          <div id="containeurTextFeature">
             <h3>Features</h3>
             <h1>Explores Our Innovative Features</h1>
           </div>
           <p>Our app is designed to make food management effortless. With features like inventory alerts and shopping list creation, you'll never run out of essentials. Plus, barcode scanning simplifies tracking your ingredients.</p>
       </div>
-      <div className="bloc">
-        <div id="containeurFeature">
-          <div className="feature">
-            <img src="" alt="" />
-            <h2>Stay Update with Inventory Alerts</h2>
-            <p>Recieve timely notifications when your stock is low</p>
+      <img id="infoImage" src="src/assets/images/Home/info.png" alt="" />
+      <div id="containeurChefReviews">
+          <div id="gauche">
+            <h1>Chef Reviews</h1>
+            <p>Find out what others think of our recipes!</p>
+            <img src="src/assets/images/Home/fourchette.png" alt="" />
           </div>
-        </div>
+          <div id="droite">
+            <img src="src/assets/images/Home/avis.png" alt="" />
+          </div>
       </div>
     </div>
   );
